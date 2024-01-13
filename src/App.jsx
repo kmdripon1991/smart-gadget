e13d074208845dc5ce4949f22cf1227b9e84ecf3
kmdripon1991 <kmdripon1991@gmail.com>
2024-01-13 15:11:48 +0600

    home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import Header from './components/Header';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
-      <Header/>
-      <Outlet/>
+      <Header />
+      <div className="min-h-[calc(100vh-96px)]">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="divide-y text-gray-800">
+      <div className="py-5 text-sm text-center text-gray-400">© 2024 SmartGadget Inc. All rights reserved.</div>
+    </footer>
+  );
+};
+
+export default Footer;
