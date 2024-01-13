3415da27fc05b4b9810d1ba3bcee4ce52a427bcb
kmdripon1991 <kmdripon1991@gmail.com>
2024-01-13 16:26:08 +0600

    shop updated

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import SingleProduct from "./SingleProduct";
 
-const [cart, setCart] = useState();
-
 const Shop = () => {
   const products = useLoaderData();
-  const handleAddToCart = product =>{
-    console.log('btn clicked')
-    }
-  }
+
+  const handleAddToCart = (product) => {
+    console.log("btn clicked");
+  };
+
   return (
     <div className="my-container">
       <div className="product-container">
         {products.map((product) => (
-          <SingleProduct key={product.id} product={product} handleAddToCart={handleAddToCart}></SingleProduct>
+          <SingleProduct
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></SingleProduct>
         ))}
       </div>
     </div>
